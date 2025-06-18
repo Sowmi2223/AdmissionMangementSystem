bdf822b18c6b7c2f2dd27db666ffadecebb1716a
Sowmiya <146162456+Sowmi2223@users.noreply.github.com>
2025-06-18 12:41:08 +0530

    Delete sample2.js

--- sample2.js
+++
@@ -1,9 +0,0 @@
-let time=0;
-        let timer=setInterval(function(){
-            time+=3;
-            document.getElementById("load").innerHTML="loading..."+time+"completed";
-            if(time>=20){
-                clearInterval(timer);
-                document.getElementById("load").innerHTML="page loaded";
-            }
-        },3000)
