3a61ced96222adc3d28250b4656bc9dfabe28f1b
Sowmiya <146162456+Sowmi2223@users.noreply.github.com>
2025-06-18 12:40:20 +0530

    Delete addition.js

--- addition.js
+++
@@ -1,9 +0,0 @@
-//function addition(){
-//let val1=parseInt(document.getElementById("val1").value);
-//let val2=parseInt(document.getElementById("val2").value);
-//console.log(val1+val2);
-//window.alert(val1+val2);
-//}
-function addition(val1,val2) {
-  return val1+val2;
-}
